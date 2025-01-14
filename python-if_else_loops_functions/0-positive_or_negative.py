3291e9ffeaae5491e2ac793f6dddce2de2528c5a
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-15 09:43:15 +1100

    Fix typo for 0

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10, 10)
 
 if number == 0:
-    print("{} is 0".format(number))
+    print("{} is zero".format(number))
 elif number < 0:
     print("{} is negative".format(number))
 else:
