f298524c73ee9675a0825fe86008fe1fba90b57e
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-16 13:45:18 +1100

    Try deleting word print from function def

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(s):
-    """prints a string in uppercase, followed by a new line."""
+    """Converts a string to uppercase, followed by a new line."""
     for c in s:
         if (ord(c) >= 97 and ord(c) <= 122):
             c = chr(ord(c) - 32)
