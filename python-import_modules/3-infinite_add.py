11e34d404c318084ed3d40e54c96796e0aa7120d
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-17 14:41:37 +1100

    Add condition for 0 args

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
     length = len(sys.argv)
     sum = 0
 
-    if length > 1:
+    if length == 0:
+        print("0")
+
+    else:
         for i in range(1, length):
             sum += int(sys.argv[i])
         print("{}".format(sum))
