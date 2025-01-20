1dc19fe030ff94cea9c0aca4a79c76a0944aa902
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-21 10:44:16 +1100

    Fix: account for end spaces

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
+        length = len(i)
+        counter = 1
         for j in i:
-            print("{:d}".format(j), end=" ")
-        print()
+            if counter < length:
+                print("{:d}".format(j), end=" ")
+            else:
+                print("{:d}".format(j))
+            counter += 1
