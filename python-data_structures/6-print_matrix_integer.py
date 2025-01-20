67a986f1a7818c93eb27a4aa64ee29987ef9a14c
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-21 10:51:47 +1100

    Account for empty matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        length = len(i)
-        counter = 1
-        for j in i:
-            if counter < length:
-                print("{:d}".format(j), end=" ")
-            else:
-                print("{:d}".format(j))
-            counter += 1
+    if matrix == [[]]:
+        print()
+    else:
+        for i in matrix:
+            length = len(i)
+            counter = 1
+            for j in i:
+                if counter < length:
+                    print("{:d}".format(j), end=" ")
+                else:
+                    print("{:d}".format(j))
+                counter += 1
