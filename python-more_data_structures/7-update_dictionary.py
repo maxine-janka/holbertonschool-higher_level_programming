4e2dc3093572ee5d3af2b4256b0e46cc5237de88
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-22 09:45:12 +1100

    A function that replaces or adds key/value in a dictionary

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary.update({key: value})
+    return a_dictionary
+
+# update() will modify existing or add if it doesnt exist.
