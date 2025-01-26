94016e5f19be598a35c79b5d74bb191732a02ac2
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-26 15:47:33 +1100

    A function that divides all elements of a matrix and doctests

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+This module supplies one function, `matrix_divided(matrix, div)`,
+that divides all elements of a matrix.
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Divides all elements of a matrix
+
+    Args:
+        matrix: (list). A list of lists of integers or floats to dicvide.
+        b: (int or float) The divisor.
+
+    Returns:
+        The resulting new list.
+    """
+    new_list = []
+    error_msg = "matrix must be a matrix (list of lists) of integers/floats"
+
+    # Matrix must be a 2D list
+    if not isinstance(matrix, list):
+        raise TypeError(error_msg)
+
+    # Rows in the list must be a list
+    for row in matrix:
+        # Rows must be same size
+        if len(row) != len(matrix[0]):
+            raise TypeError(
+                "Each row of the matrix must have the same size")
+        # Rows must me a list
+        if not isinstance(row, list):
+            raise TypeError(error_msg)
+
+        # Elements in the row must be an in or float
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError(error_msg)
+
+        # div must be an int or float
+        if not isinstance(div, (int, float)):
+            raise TypeError("div must be a number")
+
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+
+    # element = i, otherwise line space too long
+    new_list = [[round(i / div, 2) for i in row] for row in matrix]
+    return new_list
