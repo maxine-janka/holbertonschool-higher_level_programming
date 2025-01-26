e5961aadece4ed95d7e6fd13e16201e00527b751
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-26 16:36:26 +1100

    A function that prints My name is <first name> <last name>

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module supplies one function, `say_my_name(forst_name, last_name="")`,
+that prints "My name is <first name> <last name>"
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is <first name> <last name>"
+
+    Args:
+        first_name: (str). Given string for first name.
+        last_name: (str) Given string for last name, default is empty "".
+    """
+    if not isinstance (first_name, str):
+        raise TypeError("first_name must be a string")
+    elif not isinstance (last_name, str):
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
+
