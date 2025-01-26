9b759f3c8a9c3383255e887c135b30c34a3e40e4
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-26 16:52:40 +1100

    Fix typos

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module supplies one function, `say_my_name(forst_name, last_name="")`,
+This module supplies one function, `say_my_name(first_name, last_name="")`,
 that prints "My name is <first name> <last name>"
 """
 
@@ -10,13 +10,12 @@ def say_my_name(first_name, last_name=""):
     Prints "My name is <first name> <last name>"
 
     Args:
-        first_name: (str). Given string for first name.
-        last_name: (str) Given string for last name, default is empty "".
+        first_name (str):. Given string for first name.
+        last_name (str): Given string for last name, default as empty string.
     """
-    if not isinstance (first_name, str):
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    elif not isinstance (last_name, str):
+    elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     else:
         print("My name is {} {}".format(first_name, last_name))
-
