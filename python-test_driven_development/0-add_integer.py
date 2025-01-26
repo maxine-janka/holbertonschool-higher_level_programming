6c1fa86333ba7f194c6ac4f30fbba3361f6d0261
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-26 11:42:24 +1100

    A function that adds two ints and doctests

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    Adds two integers and returns the sum.
+
+    Args:
+        a: (int) The first integer.
+        b: (int) The second integer.
+
+    Returns:
+        int: The sum of a and b.
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    result = int(a) + int(b)
+    return result
