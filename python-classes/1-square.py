6754466e0da76a2990d9a15d55aae47024120281
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-28 10:54:30 +1100

    A class square with provate attribute of size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""0-square
+    This module contains the class square"""
+
+
+class Square:
+    """This is a square"""
+    def __init__(self, size):
+        """This is the size of a square"""
+        self.__size = size
