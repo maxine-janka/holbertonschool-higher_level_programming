85700f3eb938eebe7aa4aaf7c2993e442301fdca
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-28 12:13:29 +1100

    Fix typo

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -28,5 +28,5 @@ class Square:
         if not isinstance(value, int):
             raise TypeError("value must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("value must be >= 0")
         self.__size = value
