608b15b9a0eb39c23d1204022abcd2674765e969
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-28 11:19:25 +1100

    A class that defines a square with size attribute

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""0-square
+    This module contains the class square"""
+
+
+class Square:
+    """This is a definition of a square
+        Attributes:
+            size (int): Contains the length of a side of a square
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
