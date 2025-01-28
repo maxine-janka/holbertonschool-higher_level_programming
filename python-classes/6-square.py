eae78a1c1e7b8826ef9b76994dbf80ba718d51d0
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-28 14:11:16 +1100

    Add condition to get for int in tuple for position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -62,6 +62,8 @@ class Square:
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(i, int) for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
