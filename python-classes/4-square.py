3efa3a569ad8f170daaf52d5295d056c80c5c908
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-28 12:14:54 +1100

    size typo

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -26,7 +26,7 @@ class Square:
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("value must be an integer")
+            raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError("value must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = value
