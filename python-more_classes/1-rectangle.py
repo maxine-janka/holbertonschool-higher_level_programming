323afe941b673ca79a9dda4e6222d7a53c9cdaa3
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-30 09:29:19 +1100

    Fix typo in TypeError msg

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
     @width.setter
     def width(self, width):
         if not isinstance(width, int):
-            raise TypeError("width must be an interger")
+            raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
         self.__width = width
@@ -27,7 +27,7 @@ class Rectangle:
     @height.setter
     def height(self, height):
         if not isinstance(height, int):
-            raise TypeError("height must be an interger")
+            raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
