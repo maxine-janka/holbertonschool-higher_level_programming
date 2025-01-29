5c5a477c686e478d96b458e1947c387b496af787
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-30 09:11:34 +1100

    Rectangle with width and length

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""1-rectangle is a module containing the class Rectangle"""
+
+
+class Rectangle:
+    """A class that defines a Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an interger")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an interger")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
