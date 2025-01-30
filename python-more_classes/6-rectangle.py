d66df5c42e0e72cb8f6fcdc05e0a57513f897b60
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-30 11:56:45 +1100

    Add class method to count instances of Rectangle

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""1-rectangle is a module containing the class Rectangle"""
+
+
+class Rectangle:
+    """A class that defines a Rectangle"""
+    number_of_instances = 0
+    def __init__(self, width=0, height=0):
+        self.width = width  # Use the setters to validate
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @classmethod
+    def get_number_of_instances(cls):
+        return cls.number_of_instances
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        return "\n".join("#" * self.width for _ in range(self.height))
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
