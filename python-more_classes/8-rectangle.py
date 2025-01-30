71e90374afc7ce0ac306d3e6178d39fc2fde8241
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-31 10:11:00 +1100

    Fix syntax error raise not return

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -10,9 +10,9 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
-            return TypeError("rect_1 must be an instance of Rectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
-            return TypeError("rect_2 must be an instance of Rectangle")
+            raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
         else:
