6d53cdb2c53e3416c1da069503937f80e00af087
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-02-03 14:52:51 +1100

    Add docstring for Class

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""This module 1-my_list supplies a class `MyList`, with a method that
+"""This module 1-my_list supplies a class `MyList`, with a single method that
 prints a sorted list in ascending order.
 """
 
 
 class MyList(list):
+    """A class MyList that inherits from list"""
     def print_sorted(self):
         """Prints a copy of a list in ascending order"""
         print(sorted(self))
