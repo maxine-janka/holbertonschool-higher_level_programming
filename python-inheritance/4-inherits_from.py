06ca813eb8697115f32267291008cc1c7e3ece40
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-02-04 10:47:40 +1100

    Check if obj is subclass

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module ``4-inherits_from`` supplies a single method that
+checks if the object is an instance of a class that inherited
+(directly or indirectly) from a given class.
+"""
+
+
+def inherits_from(obj, a_class):
+    """Check if an object is an instance of a class (direct or indirect)"""
+    if issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
