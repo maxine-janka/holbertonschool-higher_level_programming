563bb980211a5bb7a4827faf6baf024028ba5c98
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-02-04 17:20:27 +1100

    Add docstrings

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,8 +7,15 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Defines a Rectangle"""
+    """Defines a Rectangle from BaseGeometry"""
     def __init__(self, width, height):
+        """Initialises a Rectangle.
+
+        Args:
+        width (int): The width of a new Rectangle.
+        height (int): The height of a new Rectangle.
+        
+        """
         super().integer_validator("width", width)
         self.__width = width
         super().integer_validator("height", height)
