b960e30c4bf410701551ea5fc492a34a48a9851d
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-02-04 18:46:40 +1100

    Change isinstance to type, test was not working with bool

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validates whether value is an integer"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
