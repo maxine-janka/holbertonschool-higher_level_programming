0ce9c098e47a041d7cb286a1f194a102007de916
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-02-04 13:12:18 +1100

    fix format string for exception error msgs

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,6 +13,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Validates whether value is an integer"""
         if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
