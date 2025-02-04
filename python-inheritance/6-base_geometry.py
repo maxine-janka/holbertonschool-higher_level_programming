a02d3966f3bd853ce215cf723d865c3450842446
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-02-04 11:27:27 +1100

    An area method not in use

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module ``base_geometry`` supplies an empty class BaseGeometry
+and a method area that is not implemented"""
+
+
+class BaseGeometry:
+    """An empty class"""
+
+    def area(self):
+        """An area method that is not implemented"""
+        raise Exception("area() is not implemented")
