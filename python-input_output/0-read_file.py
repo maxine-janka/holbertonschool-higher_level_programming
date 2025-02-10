cbd78af9c9e4c1a625e8138716e1d11011fb226b
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-02-10 12:51:42 +1100

    Fix add module docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""A function that reads a text file"""
 
 def read_file(filename=""):
-    """Reads a text file and prrints it to stdout"""
+    """Reads a text file and prints it to stdout"""
     with open(filename, encoding='UTF-8') as a_file:
         print_content = a_file.read()
         print(print_content, end='')
