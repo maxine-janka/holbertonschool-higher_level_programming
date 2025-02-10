2264b77d1c7309595113acf95627bd7e0cd08043
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-02-10 12:52:32 +1100

    py style fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A function that reads a text file"""
 
+
 def read_file(filename=""):
     """Reads a text file and prints it to stdout"""
     with open(filename, encoding='UTF-8') as a_file:
