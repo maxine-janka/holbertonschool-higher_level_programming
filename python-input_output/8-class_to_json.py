7107626a785eb8ed2da291a2563f48118e5227ae
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-02-13 10:43:19 +1100

    Remove json module

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 "Defines a function that converts an instance of a class to a dictionary"
 
-import json
-
 
 def class_to_json(obj):
     """Returns the dictionary representation of an object's attributes"""
