b3ed0f2847f080d675fbb7d6b9efaf930d7985b0
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-02-14 16:24:00 +1100

    pickle unpickle custom Python objects

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Serialize and Deserialize using pickle"""
+import pickle
+
+
+class CustomObject:
+    """Defines a custom object"""
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Prints attributes in the given format"""
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        """Serializes the current instance of an object"""
+        try:
+            with open(filename, mode='wb') as file:
+                return pickle.dump(file)
+        except pickle.PickleError:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserializes and returns the object"""
+        try:
+            with open(filename, mode='rb') as file:
+                return pickle.load(file)
+        except pickle.UnpicklingError:
+            return None
+        except FileExistsError:
+            return None
