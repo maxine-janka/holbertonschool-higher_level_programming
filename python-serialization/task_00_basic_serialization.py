7283059674c34088a70f309f57414db9b0f82107
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-02-14 15:58:51 +1100

    Change pickle to json for simple data types

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """A basic serialization module"""
 
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
     """Serilizes the given data and saves to a file"""
-    with open(filename, mode="wb") as file:
-        pickle.dump(data, file)
+    with open(filename, mode="w") as file:
+        json.dump(data, file)
 
 
 def load_and_deserialize(filename):
     """Deserialise and load data from a given file"""
-    with open(filename, mode="rb") as file:
-        return pickle.load(file)
+    with open(filename, mode="r") as file:
+        return json.load(file)
