7685a80510d71cb0da3d915363aa4672c79a67b9
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-04 15:30:52 +1100

    Only display score and name - in this order

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- Lists all records of the table named second_table of the database hbtn_0c_0
 -- Lists all records in descending order by score
-SELECT * 
+SELECT score, name
 FROM second_table
 ORDER BY score DESC;
