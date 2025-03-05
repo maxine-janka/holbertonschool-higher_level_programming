fae68bd4ab0c4af390021c063c976f3887746a14
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-05 11:43:16 +1100

    count on same line, add order by

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- Lists the number of records with the same 'score' in the table named second_table
-SELECT score,
-COUNT(*) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
-GROUP BY score DESC;
+GROUP BY score
+ORDER BY score DESC;
