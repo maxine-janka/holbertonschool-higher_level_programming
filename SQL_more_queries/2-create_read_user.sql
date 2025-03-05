3f6ca1ddb9a519aabb114cf6b93ba4fafdfbfde8
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-06 10:04:06 +1100

    Initial commit: New DB, new user with select privleges

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Creates a new database named hbtn_0d_2, and a new user with SELECT privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
