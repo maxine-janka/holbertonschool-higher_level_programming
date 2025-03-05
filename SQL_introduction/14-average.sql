1dfbed9dea14b071c9dadefa4b45a943381f8c2d
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-05 11:26:47 +1100

    Initial commit: compute the avg score for all records and create new column

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Computes the 'score' average for all records in the table named second_table and creates a new column called 'average'.
-SELECT AVG(score)
+SELECT AVG(score) AS 'average'
 FROM second_table
