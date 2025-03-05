6492ad698810f628e0faf5aa72bb6982aaf09e39
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-06 09:59:36 +1100

    Add semicolon to first line

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- Creates a user with global privledges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'; -- Global privledges, all databases and all tables
+-- Creates a user with global privleges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'; -- Global privleges, all databases and all tables
