34af5959a5c6008edd536d1c9b34b65aad2a508f
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-06 09:54:39 +1100

    Add opening comment

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
+-- Creates a user with global privledges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'; -- Global privledges, all databases and all tables
