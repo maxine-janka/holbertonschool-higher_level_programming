35435a9a6bbc9f5c489a57800a5a8d8229d68aad
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-06 11:12:56 +1100

    Initial commit: Create DB and table, id column is  unique, not null, auto-gen & starts at 1

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Creates a new database named hbtn_0d_usa, and a table names states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
+-- AUTO_INCREMENT automatically generates a new id, starting at 1
+-- PRIMARY KEY implies NOT NULL and unique
