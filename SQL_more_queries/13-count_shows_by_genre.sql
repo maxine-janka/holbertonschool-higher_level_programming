64194b54cb8d9ac7b7c1c54173b7848a4fac315f
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-06 14:49:00 +1100

    Fix uppercase

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
  -- Lists all genres in the database hbtn_0d_tvshows that have atleast one show linked
  SELECT tv_genres.name,
-        COUNT(tv_show_genres.genre_id) as number_of_shows
+        COUNT(tv_show_genres.genre_id) AS number_of_shows
  FROM tv_genres
  LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
  GROUP BY tv_genres.name
