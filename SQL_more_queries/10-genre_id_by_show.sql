fcf7d4a6fc85b726b606c1163c66682872d42337
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-06 14:04:12 +1100

    Add space for comments

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -4,7 +4,7 @@
  JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
  ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
 
---SELECT column to display
---FROM a table, can be tv_shows or tv_show_genres
---JOIN other table. Grab the id from the tv_shows table and find the genre with that show_id in the tv_show_genres table.
+-- SELECT column to display
+-- FROM a table, can be tv_shows or tv_show_genres
+-- JOIN other table. Grab the id from the tv_shows table and find the genre with that show_id in the tv_show_genres table.
 -- ORDER BY title and genre id within the title.
