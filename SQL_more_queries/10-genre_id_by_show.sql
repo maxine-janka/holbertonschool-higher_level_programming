74a4ae1de144476a5c442c84691186c452b7719c
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-06 14:01:29 +1100

    Initial commit: List all tv shows with a linked genre

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,10 @@
+ -- Lists all shows in the database hbtn_0d_tvshows that have atleast one genre linked
+ SELECT tv_shows.title, tv_show_genres.genre_id
+ FROM tv_shows
+ JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+
+--SELECT column to display
+--FROM a table, can be tv_shows or tv_show_genres
+--JOIN other table. Grab the id from the tv_shows table and find the genre with that show_id in the tv_show_genres table.
+-- ORDER BY title and genre id within the title.
