cea76e8d350e1a448d2ae8928a259cef12f4bcd8
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-06 14:48:14 +1100

    Initial commit: list all genres with number of shows linked, exclude NULL

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,8 @@
 --  -- Lists all shows in the database hbtn_0d_tvshows that have a linked genre, if no genre display NULL
- SELECT tv_shows.title, tv_show_genres.genre_id
+ SELECT tv_shows.title, tv_show_genres.genre_id,
+        COUNT(tv_show_genres.genre_id) as number_of_shows
  FROM tv_shows
  LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
  ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
 
  -- JOIN = INNER JOIN, LEFT JOIN to include all rows from tv_shows even if there is no matching genre.
- 
---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+ -- Lists all genres in the database hbtn_0d_tvshows that have atleast one show linked
+ SELECT tv_genres.name,
+        COUNT(tv_show_genres.genre_id) as number_of_shows
+ FROM tv_genres
+ LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+ GROUP BY tv_genres.name
+ ORDER BY number_of_shows DESC;
+
+-- COUNT requires a GROUP BY to group the count by genre name
