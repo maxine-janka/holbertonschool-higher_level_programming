0b3c2ee40f978f5d20e67051eb0d1d80549487e2
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-06 14:52:00 +1100

    Change to inner join to exclude 0

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,8 +2,9 @@
  SELECT tv_genres.name,
         COUNT(tv_show_genres.genre_id) AS number_of_shows
  FROM tv_genres
- LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+ INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
  GROUP BY tv_genres.name
  ORDER BY number_of_shows DESC;
 
 -- COUNT requires a GROUP BY to group the count by genre name
+-- INNER JOIN only include rows where there is a match
