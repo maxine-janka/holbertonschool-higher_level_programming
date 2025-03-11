b0c596a6a4bbab232133a66941fe6fabec7df540
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-11 11:32:27 +1100

    Add conditional to run as main program

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,28 +4,30 @@
 import sys
 import MySQLdb
 
-# connect to a mysql database with args provided when running the script
-db = MySQLdb.connect(
-    host='localhost',
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3],
-)
+if __name__ == "__main__":
 
-# Like a pointer that sends commands to the db to retrieve the results
-cursor = db.cursor()
+    # connect to a mysql database with args provided when running the script
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
 
-# Cursor sends query to db
-cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+    # Like a pointer that sends commands to the db to retrieve the results
+    cursor = db.cursor()
 
-# fetch all rows from states table and store in 'states' as a list of tuples
-states = cursor.fetchall()
+    # Cursor sends query to db
+    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
 
-# Iterate rows and print
-for state in states:
-    print(state)
+    # fetch all rows from states table and store in 'states' as a list of tuples
+    states = cursor.fetchall()
 
-# Close the cursor and free up resources, close db connection
-cursor.close()
-db.close()
+    # Iterate rows and print
+    for state in states:
+        print(state)
+
+    # Close the cursor and free up resources, close db connection
+    cursor.close()
+    db.close()
