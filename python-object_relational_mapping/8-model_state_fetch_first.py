75eef29f7a7561a946f1a1730553fcc27aa6dca0
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-12 11:26:13 +1100

    Add condition for nothing

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -23,5 +23,8 @@ if __name__ == "__main__":
 
     # Query, print and close
     state = session.query(State).first()
-    print("{}: {}".format(state.id, state.name))
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
     session.close()
