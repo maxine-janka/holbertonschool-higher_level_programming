1eda68b377c1f008bf8c96c83e38c1e9fa334066
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-12 15:57:34 +1100

    Initial comit: delete states with letter a

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Deletes all State objects with a name containing the letter 'a'
+   from the database hbtn_0e_6_usa"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+# Don't execute when imported
+if __name__ == "__main__":
+
+    # Create engine with credentials from command line to connect to DB
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+    )
+    # Creates a table from State class that inherits from Base
+    # Dont need this line, it will create a table if its not there
+    # Base.metadata.create_all(bind=engine)
+
+    # Create interactive session with DB to make queries
+    create_session_obj = sessionmaker(bind=engine)
+    session = create_session_obj()  # Instantiate a session
+
+    # Query, print and close
+    for state in session.query(State).filter(State.name.contains('a')):
+        session.delete(state)
+    session.commit()
+
+    session.close()
