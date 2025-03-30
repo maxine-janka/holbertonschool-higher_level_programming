5271a71ebcdfcd44adb87f7aa56c18b6b1a1da9a
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-30 17:21:29 +1100

    Render list from json file

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import os
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/items')
+def items():
+     # read the JSON file
+    with open('items.json', 'r') as items_file:
+        item_data = json.load(items_file)
+
+    return render_template('items.html', items=item_data.get("items"))
+
+if __name__ == '__main__':
+       app.run(debug=True, port=5000)
